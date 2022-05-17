9f0c724b273884d0edc6b0244e9e7ed809fcdb41
VToribiot <80557014+VToribiot@users.noreply.github.com>
2022-05-17 16:22:42 -0400

    Fixing functions based on tests results

--- UnitTest.py
+++ UnitTest.py
@@ -31,16 +31,16 @@ class MyTestCase(u.TestCase):
         self.assertTrue((m.check(['357', '54', '9878'])))
 
     def test10(self):
-        self.assertFalse((m.string_validation(['13?', '48', '118/'])))
+        self.assertFalse((m.string_validation('13?-48-118/')))
 
     def test11(self):
-        self.assertFalse((m.string_validation(['12', '5', '384'])))
+        self.assertFalse((m.string_validation('12-5-384')))
 
     def test12(self):
-        self.assertFalse((m.string_validation(['159', 'w4', '3848'])))
+        self.assertFalse((m.string_validation('159-w4-3848')))
 
     def test13(self):
-        self.assertTrue((m.string_validation(['123', '45', '6789'])))
+        self.assertTrue((m.string_validation('123-45-6789')))
 
 
 def run():
